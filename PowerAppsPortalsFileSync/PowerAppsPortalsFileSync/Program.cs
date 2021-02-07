9e7a809c31ce20120c22f7f2690098e082f4576c
Joris Kalz <joris@kalz.de>
2021-02-07 20:16:09 +0100

    Building Connection String

--- PowerAppsPortalsFileSync/PowerAppsPortalsFileSync/Program.cs
+++ PowerAppsPortalsFileSync/PowerAppsPortalsFileSync/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLine;
+using PowerApps.Samples;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,17 @@ namespace PowerAppsPortalsFileSync
 {
     class Program
     {
+        static string connectionString = string.Empty;
+        static ServiceConfig config;
+
         static void Main(string[] args)
         {
             Parser.Default.ParseArguments<Options>(args)
                .WithParsed<Options>(o =>
                {
+                   connectionString = $"Url={o.Url};Authority=null;UserPrincipalName={o.Email};Password={o.Password};CallerObjectId=null;Version=9.1;MaxRetries=3;TimeoutInSeconds=180;";
+                   config = new ServiceConfig(connectionString);
+
                    try
                    {
                        
