9ab641df32393f6e080a051414fa5cf9b918aa88
Joris Kalz <joris@kalz.de>
2021-02-07 20:38:39 +0100

    Loading Languages

---
+++ PowerAppsPortalsFileSync/PowerAppsPortalsFileSync/Model/PortalLanguage.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerAppsPortalsFileSync.Model
+{
+    public class PortalLanguage
+    {
+        [JsonProperty("adx_portallanguageid")]
+        public Guid PortalLanguageId { get; set; }
+
+        [JsonProperty("adx_languagecode")]
+        public string LanguageCode { get; set; }
+    }
+}
--- PowerAppsPortalsFileSync/PowerAppsPortalsFileSync/Program.cs
+++ PowerAppsPortalsFileSync/PowerAppsPortalsFileSync/Program.cs
@@ -1,5 +1,7 @@
 ﻿using CommandLine;
+using Newtonsoft.Json.Linq;
 using PowerApps.Samples;
+using PowerAppsPortalsFileSync.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,21 +20,48 @@ namespace PowerAppsPortalsFileSync
             Parser.Default.ParseArguments<Options>(args)
                .WithParsed<Options>(o =>
                {
-                   connectionString = $"Url={o.Url};Authority=null;UserPrincipalName={o.Email};Password={o.Password};ClientId=12f81219-12ee-4a9e-12ae-a2591f43381d;RedirectUrl=app://12f81219-12ee-4a9e-12ae-a2591f43381d;CallerObjectId=null;Version=9.1;MaxRetries=3;TimeoutInSeconds=180;";
+                   connectionString = $"Url={o.Url};Authority=null;ClientId=51f81489-12ee-4a9e-aaae-a2591f45987d;RedirectUrl=app://58145B91-0C36-4500-8554-080854F2AC97;UserPrincipalName={o.Email};Password={o.Password};CallerObjectId=null;Version=9.1;MaxRetries=3;TimeoutInSeconds=180;";
+
                    config = new ServiceConfig(connectionString);
 
                    try
                    {
-                       
+                       processPortal(config, o.Folder, o.Save);
+
                    }
-                   catch (Exception)
+                   catch (Exception ex)
                    {
-                       throw;
+                       Console.WriteLine($"Error: {ex.Message}");
                    }
                });
 
             Console.ReadLine();
 
+        }
+
+        private static void processPortal(ServiceConfig config, string baseFolder, bool import = false)
+        {
+            using (CDSWebApiService svc = new CDSWebApiService(config))
+            {
+
+                // Header required to include formatted values
+                var formattedValueHeaders = new Dictionary<string, List<string>> {
+                        { "Prefer", new List<string>
+                            { "odata.include-annotations=\"OData.Community.Display.V1.FormattedValue\"" }
+                        }
+                    };
+
+                Console.WriteLine("-- Start Loading Portal Languages --");
+                // adx_portallanguages?$select=adx_portallanguageid,adx_languagecode&$top=50
+                var portalLanguages = (svc.Get("adx_portallanguages?" +
+                    "$select=adx_portallanguageid,adx_languagecode",
+                        formattedValueHeaders)["value"] as JArray).ToObject<PortalLanguage[]>();
+
+                foreach (var item in portalLanguages)
+                {
+                    Console.WriteLine(item.LanguageCode);
+                }
+            }
         }
     }
 }
