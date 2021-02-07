46fb3f492e7bcb451293c33679675882822c9fc3
Joris Kalz <joris@kalz.de>
2021-02-07 20:01:49 +0100

    Initial

---
+++ PowerAppsPortalsFileSync/PowerAppsPortalsFileSync/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerAppsPortalsFileSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
